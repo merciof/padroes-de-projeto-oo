1444f5b31089f843e1f123b932318e9f86670644
merciof <mercio.andrade@ufrpe.br>
2020-06-21 17:49:21 -0300

    criacao da classe Assunto e adicao do atributo estado

---
+++ src/composicao/observador/Assunto.java
@@ -0,0 +1,20 @@
+package composicao.observador;
+
+/**
+ * 
+ * @author merciof
+ *
+ */
+public class Assunto {
+	
+	private int estado;
+
+	public int getEstado() {
+		return estado;
+	}
+
+	public void setEstado(int estado) {
+		this.estado = estado;
+	}
+}
+
