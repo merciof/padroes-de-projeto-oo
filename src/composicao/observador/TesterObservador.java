41f109aa35a843561840e87b529daa7d93ab3fc3
merciof <mercio.andrade@ufrpe.br>
2020-06-21 18:08:05 -0300

    adicao de teste para a adicao e romoção de um observador

--- src/composicao/observador/TesterObservador.java
+++ src/composicao/observador/TesterObservador.java
@@ -9,8 +9,29 @@ public class TesterObservador {
 	@Test
 	public void alteracaoEstado() {
 		Assunto assunto = new Assunto();
-		assunto.setEstado(0);
+		//assunto.setEstado(0);
 		assertEquals(0, assunto.getEstado());
 	}
+	
+	@Test
+	public void adicionarUmObservador() {
+		Assunto assunto = new Assunto();
+		Observador observador = new Binario();
+		assunto.adicionarObservador(observador);
+		assertEquals(1, assunto.observadores.size());
+	}
+	
+	@Test
+	public void removerUmObservador() {
+		Assunto assunto = new Assunto();
+		Observador observador = new Binario();
+		assunto.adicionarObservador(observador);
+		assertEquals(1, assunto.observadores.size());
+		assunto.removerObservador(observador);
+		assertEquals(0, assunto.observadores.size());
+		
+	}
+	
+	
 
 }
