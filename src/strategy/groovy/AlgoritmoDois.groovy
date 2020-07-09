81c6219b9beee2352a248522f639ba75b6ac4d91
merciof <mercio.andrade@ufrpe.br>
2020-07-09 20:31:45 -0300

    adição de classes com algoritmos

---
+++ src/strategy/groovy/AlgoritmoDois.groovy
@@ -0,0 +1,10 @@
+package strategy.groovy
+
+class AlgoritmoDois implements Calculadora {
+
+	@Override
+	public def calcular() {
+		// TODO Auto-generated method stub
+		return "Executando algoritmo Dois!";
+	}
+}
---
+++ src/strategy/groovy/AlgoritmoUm.groovy
@@ -0,0 +1,10 @@
+package strategy.groovy
+
+class AlgoritmoUm implements Calculadora {
+
+	@Override
+	public Object calcular() {
+		// TODO Auto-generated method stub
+		return "Executando algoritmo Um!";
+	}
+}
