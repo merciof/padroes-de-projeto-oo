2c095fd897db77f1ba437e1a8097a332e231aad4
merciof <mercio.andrade@ufrpe.br>
2020-07-09 20:30:35 -0300

    adição de classes com algoritmos

---
+++ src/strategy/java/AlgoritmoDois.java
@@ -0,0 +1,11 @@
+package strategy.java;
+
+public class AlgoritmoDois implements Calculadora {
+
+	@Override
+	public String calcular() {
+		// TODO Auto-generated method stub
+		return "Executando algoritmo dois.";
+	}
+
+}
---
+++ src/strategy/java/AlgoritmoUm.java
@@ -0,0 +1,11 @@
+package strategy.java;
+
+public class AlgoritmoUm implements Calculadora {
+
+	@Override
+	public String calcular() {
+		
+		return "Executando algoritmo um.";
+	}
+
+}
