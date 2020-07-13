64aa94bdb7d3c8139db21ca4bc52504579cc4f47
merciof <mercio.andrade@ufrpe.br>
2020-07-13 11:04:01 -0300

    criação da classe triangulo

---
+++ src/criacional/factory/Triangulo.java
@@ -0,0 +1,11 @@
+package criacional.factory;
+
+public class Triangulo implements IForma {
+
+	@Override
+	public String draw() {
+		// TODO Auto-generated method stub
+		return "Executando desenho de Triangulo";
+	}
+
+}
