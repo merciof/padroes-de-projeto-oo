ce074dbfec62ee9cc8a0e8cb8d4903ec4f3bb1c5
merciof <mercio.andrade@ufrpe.br>
2020-07-14 09:35:03 -0300

    criação da classe gamao

---
+++ src/comportamental/TemplateMethod/Gamao.java
@@ -0,0 +1,23 @@
+package comportamental.TemplateMethod;
+
+public class Gamao extends Jogo {
+
+	@Override
+	void iniciar() {
+		// TODO Auto-generated method stub
+		System.out.println("Iniciando jogo de Gamao!"); 
+	}
+
+	@Override
+	void jogar() {
+		// TODO Auto-generated method stub
+		System.out.println("Jogando jogo de Gamao!"); 
+	}
+
+	@Override
+	void finalizar() {
+		// TODO Auto-generated method stub
+		System.out.println("Finalizando Gamao!"); 
+	}
+
+}
