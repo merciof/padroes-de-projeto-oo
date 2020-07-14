0028345a650cda5751ecdf71c87f19285e33b25c
merciof <mercio.andrade@ufrpe.br>
2020-07-14 09:35:23 -0300

    criação da classe Xadrez

---
+++ src/comportamental/TemplateMethod/Xadrez.java
@@ -0,0 +1,23 @@
+package comportamental.TemplateMethod;
+
+public class Xadrez extends Jogo {
+
+	@Override
+	void iniciar() {
+		// TODO Auto-generated method stub
+		System.out.println("Iniciando jogo de Xadrez!");
+	}
+
+	@Override
+	void jogar() {
+		// TODO Auto-generated method stub
+		System.out.println("Jogando jogo de Xadrez!"); 
+	}
+
+	@Override
+	void finalizar() {
+		// TODO Auto-generated method stub
+		System.out.println("Finalizando jogo de Xadrez!"); 
+	}
+
+}
