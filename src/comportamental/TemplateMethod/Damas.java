aeae24efd4232127e062986b4d29c454369adf7f
merciof <mercio.andrade@ufrpe.br>
2020-07-14 09:35:11 -0300

    criação da classe damas

---
+++ src/comportamental/TemplateMethod/Damas.java
@@ -0,0 +1,22 @@
+package comportamental.TemplateMethod;
+
+public class Damas extends Jogo {
+
+	@Override
+	void iniciar() {
+		// TODO Auto-generated method stub
+		System.out.println("Iniciando jogo de Damas!"); 
+	}
+
+	@Override
+	void jogar() {
+		// TODO Auto-generated method stub
+		System.out.println("Jogando jogo de Damas!"); 
+	}
+
+	@Override
+	void finalizar() {
+		// TODO Auto-generated method stub
+		System.out.println("Finalizando Damas!"); 
+	}
+}
